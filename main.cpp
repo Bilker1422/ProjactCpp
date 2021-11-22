3ceffdc214ca651ddee8ca9497b17a04c752428a
Bilker2 <Yahyaksa28@gmail.com>
2021-11-22 11:55:35 +0300

    struct have been created

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#define MAX 6
+using namespace std;
+
+
+struct Courses{
+    int CRN;
+    string course;
+};
+struct Student{
+    int studentID;
+    string studentName;
+    int studentLevel;
+    float GPA;
+    Courses list[MAX];
+};
+struct Node{
+    Student data;
+    Node *pre;
+    Node *next;
+};
+
+int main(){
+
+}
