fc877b11ca59ded1143a4b5be32c0c18e12a8fa8
Ahm2dd <94544270+Ahm2dd@users.noreply.github.com>
2021-12-01 18:21:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,6 +21,62 @@ struct Node{
     Node *next;
 }*tail=NULL,*head=NULL;
 
+CreateEmpty(){
+	Node *temp;
+	temp = new Node;
+	
+	
+	cout <<"Enter the name :";
+	cin>>temp->data.studentID;
+	cout<<"\nEnter the ID :";
+	cin>>temp->data.studentID;
+	cout<<"\nEnter the GPA :";
+	cin>>temp->data.GPA;
+	cout<<"\nEnter the Level:";
+	cin>>temp->data.studentLevel;
+	cout<<"\n Courses :";
+	for(int x = 0; x >=5; x++){
+
+		temp->data.list[x];
+		
+	cout << " Enter the CRN:";
+		
+	
+		cout << " Enter the name of the course:";
+	
+		
+
+		}
+	
+	temp->next=NULL;
+	temp->pre = NULL;
+	
+	};
+	void InsertRear(Node *tail , Node *head,Node *temp){
+		
+		CreateEmpty();
+		
+		if(head == NULL){
+			head = temp;
+			temp->next=NULL;
+			temp->pre=NULL;
+			tail = temp;
+		}
+		else
+		{
+			temp->pre= tail;
+			tail->next= temp;
+			temp->next=NULL;
+			tail= temp;
+			
+			
+		}
+		
+		
+		
+		
+}
+	
 int main(){
 
 }
