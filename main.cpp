7edd6767b37f1723ebc6510b8d6568d70063f005
Bilker2 <Yahyaksa28@gmail.com>
2021-12-02 03:19:12 +0300

    Updated

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <string>
-#define MAX 6
+#include <limits>
+
 using namespace std;
 
 
-struct Courses{
+struct Course{
     int CRN;
-    string course;
+    Course *next = NULL;
 };
 struct Student{
     int studentID;
     string studentName;
     int studentLevel;
     float GPA;
-    Courses list[MAX];
+    Course *list;
 };
 struct Node{
     Student data;
@@ -21,62 +22,90 @@ struct Node{
     Node *next;
 }*tail=NULL,*head=NULL;
 
-CreateEmpty(){
-	Node *temp;
-	temp = new Node;
-	
-	
-	cout <<"Enter the name :";
-	cin>>temp->data.studentID;
-	cout<<"\nEnter the ID :";
-	cin>>temp->data.studentID;
-	cout<<"\nEnter the GPA :";
-	cin>>temp->data.GPA;
-	cout<<"\nEnter the Level:";
-	cin>>temp->data.studentLevel;
-	cout<<"\n Courses :";
-	for(int x = 0; x >=5; x++){
+Course *List(int n){
+    Course *Head = new Course;
+    Head ->next=NULL;
 
-		temp->data.list[x];
-		
-	cout << " Enter the CRN:";
-		
-	
-		cout << " Enter the name of the course:";
-	
-		
+    if (n != 0){
+        cout << "Enter CRN: ";
+        cin >> Head ->CRN;
+        Head -> next = List(n-1); 
+    }
+    return Head;
+}
 
-		}
-	
-	temp->next=NULL;
-	temp->pre = NULL;
-	
-	};
-	void InsertRear(Node *tail , Node *head,Node *temp){
-		
-		CreateEmpty();
-		
-		if(head == NULL){
-			head = temp;
-			temp->next=NULL;
-			temp->pre=NULL;
-			tail = temp;
-		}
-		else
-		{
-			temp->pre= tail;
-			tail->next= temp;
-			temp->next=NULL;
-			tail= temp;
-			
-			
-		}
-		
-		
-		
-		
+Node *CreateEmpty(int ID)
+{
+    Node *temp = new Node;
+    temp ->next = NULL;
+    temp ->pre = NULL;
+
+    temp ->data.studentID = ID;
+    cin.ignore();
+    cout << "Name of Student: ";
+    getline(cin , temp -> data.studentName);
+    cout << "Level: ";
+    cin >> temp ->data.studentLevel;
+    cout << "GPA: ";
+    cin >> temp ->data.GPA;
+
+    int counter = -1;
+    cout << "How many coures to regsiter: ";
+    cin >> counter;
+    temp ->data.list = List(counter);
+
+    return temp;
+}
+void InsertRear(int ID){
+    Node *temp = CreateEmpty(ID);
+    if (head == NULL)
+        head = tail = temp;
+    else{
+        temp ->pre = tail;
+        tail ->next = temp;
+        tail = temp;
+    }
+    cout << endl;
 }
 	
 int main(){
+    int ID = 0;
+    int choice;
 
+    do
+    {
+        try{
+            cout << "1.To insert new Student\n2.To search for student using his ID\n3.To search for coures\n4.To search for range GPA\n5.To delete using his ID\n6.To exit\n";
+            cout <<"Your Choice: ";
+            cin >> choice;
+            switch(choice)
+            {
+                case 1:
+                    cout << endl;
+                    InsertRear(ID+1);
+                    ID++;
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                   
+                    break;
+                case 5:
+                    break;
+                    
+            }
+            if (!cin.good())
+                throw -1;
+        }
+        catch (...){
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    } while (choice != 6);
+    
 }
+
